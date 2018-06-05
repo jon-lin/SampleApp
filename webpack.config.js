90a989c876c556ef5e206ef37004723395a2a4d0
Jon Lin <jon.cp.lin@gmail.com>
2018-06-05 07:55:19 -0400

    Save Sample App working (without file loader)

--- webpack.config.js
+++ webpack.config.js
@@ -28,53 +28,25 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        // use: 'babel-loader',
         loader: 'babel-loader',
         options: {
           plugins: ['react-hot-loader/babel']
         }
       },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|txt)$/,
-        loader: 'file-loader'
-      }
+      // {
+      //   test: /\.(eot|svg|ttf|woff|woff2|txt)$/,
+      //   loader: 'file-loader'
+      // },
+      // {
+      //   test: /\.json$/,
+      //   loader: 'json-loader'
+      // },
+      // {
+      //   test: /\.(graphql|gql)$/,
+      //   exclude: /node_modules/,
+      //   loader: 'graphql-tag/loader'
+      // }
     ],
-    // loaders: [{
-    //   test: /\.css$/,
-    //   exclude: /node_modules/,
-    //   loader: 'style-loader!css-loader'
-    // }, {
-    //   // Do not transform vendor's CSS with CSS-modules
-    //   // The point is that they remain in global scope.
-    //   // Since we require these CSS files in our JS or CSS files,
-    //   // they will be a part of our compilation either way.
-    //   // So, no need for ExtractTextPlugin here.
-    //   test: /\.css$/,
-    //   include: /node_modules/,
-    //   loaders: ['style-loader', 'css-loader']
-    // }, {
-    //   test: /\.(eot|svg|ttf|woff|woff2|txt)$/,
-    //   loader: 'file-loader'
-    // }, {
-    //   test: /\.(jpg|png|gif)$/,
-    //   loaders: [
-    //     'file-loader',
-    //     'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}',
-    //   ],
-    // }, {
-    //   test: /\.html$/,
-    //   loader: 'html-loader'
-    // }, {
-    //   test: /\.json$/,
-    //   loader: 'json-loader'
-    // }, {
-    //   test: /\.(mp4|webm)$/,
-    //   loader: 'url-loader?limit=10000'
-    // }, {
-    //   test: /\.(graphql|gql)$/,
-    //   exclude: /node_modules/,
-    //   loader: 'graphql-tag/loader'
-    // }],
   },
   // ONLY enable noEmitOnErrors IN PROD, this prevents webpack bundle errors
   // from printing in browser console, and will execute code as well as it can
